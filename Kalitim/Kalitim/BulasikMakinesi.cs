8525191fb767a7cf42e913a035238b07ad3799e9
Burak SALCAN <buraksalcan@gmail.com>
2024-11-02 22:35:38 +0300

    Kalitim
    2.11.2024 2. dersi

---
+++ Kalitim/Kalitim/BulasikMakinesi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    internal class BulasikMakinesi : BeyazEsya
+    {
+        public BulasikMakinesi() { }
+        public BulasikMakinesi(string Isim, string Marka, double Fiyat, string EnergiSinifi, int Kapasite, bool YogunYikama) 
+            :base(Isim, Marka, Fiyat)
+        { 
+            this.EnerjiSinifi = EnergiSinifi;
+            this.YogunYikama = YogunYikama;
+            this.Kapasite = Kapasite;
+        }
+
+
+        public string EnerjiSinifi { get; set; }
+
+        public bool YogunYikama { get; set; }
+
+        public int Kapasite { get; set; }
+    }
+}
---
+++ Kalitim/Kalitim/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BeyazEsya esya = new BeyazEsya();
+            //esya.Isim = "Deneme";
+            //esya.Marka = "Deneme";
+            //esya.Fiyat = 50;
+
+
+
+            CamasirMakinesi cm = new CamasirMakinesi();
+            cm.Marka = "BOSCH";
+            cm.ProgramSayisi = 3;
+            cm.Kurutma = true;
+            cm.Isim = "BO131";
+            cm.Fiyat = 10000;
+
+            BulasikMakinesi bm = new BulasikMakinesi();
+            bm.Marka = "SIEMENS";
+            bm.Isim = "SIE31";
+            bm.Fiyat = 11000;
+            bm.Kapasite = 300;
+            bm.YogunYikama = true;
+            bm.EnerjiSinifi = "A+++";
+
+            List<BeyazEsya> esyalar = new List<BeyazEsya>();
+            esyalar.Add(cm);
+            esyalar.Add(bm);
+            for (int i = 0; i < esyalar.Count; i++)
+            {
+                Console.WriteLine($"{esyalar[i].Marka} {esyalar[i].Isim} {esyalar[i].Fiyat}");
+            }
+            
+
+
+        }
+    }
+}
